743842b8a36ac463824544cbf0cb8cdb5f6485e7
Daniel Zeman <Daniel.Zeman@student.souepl.cz>
2024-11-29 11:36:31 +0100

    3

--- heeeelp.js
+++ heeeelp.js
@@ -1,19 +1,44 @@
 const nabidka = ["CPU", "GPU", "RAM"];
 const kosik = [];
 
-function vypis_pole_do_elementu(array, output){
+function vypis(array, output){
     let out_value = document.getElementById(output);
     for(let i = 0; i < array.length; i++){
         out_value.innerHTML += i + 1 + ". " + array[i] + "<br>";
     }
 }
 
-vypis_pole_do_elementu(nabidka, "content");
-
-/*
-function vypis(array, output){
-}
-
 function Add(){
+    const input = document.getElementById("add-component")
+
+    if((input.ariaValueMax<=nabidka.length) && (input.value> 0) && (!isNaN(input.value))){
+        kosik.push(nabidka[input.value -1]);
+        nabidka.splice(input.value - 1,1);
+    }
+    else{
+        alert("chyba")
+    }
+    vypis(kosik, "user-cart");
+    vypis(nabidka, "content");
+    upadateKosikOptions();
 }
-*/
+
+function updateKosikOptions() { 
+    // Najdeme HTML element typu select
+    const select = document.getElementById("kosik-nabidka"); 
+    // Vymažeme jeho aktuální obsah
+    select.innerHTML = ""; 
+    
+    // Projdeme všechny položky v košíku
+    for (let i = 0; i < kosik.length; i++) { 
+        // 1. Vytvoříme nový prvek <option> pomocí příkazu createElement, do vnitř píšeme název elementu bez <>
+        let option = document.createElement("option"); 
+        // 2. Nastavíme hodnotu a zobrazovaný text, hodnota je zde atriput <option value="1"></option>
+        option.value = i + 1; 
+        option.text = kosik[i]; // Můžete použít i innerHTML, ale text je kratší, element pak vypadá jíž takto <option value="1">Nazev produktu z košíku</option>
+        // 3. Přidáme <option> do <select>, jednodušeji si select element adptuje potomka, kterého jsme vytvořili
+        select.appendChild(option); 
+    } 
+} 
+
+vypis(nabidka, "content");
